82a0044f5c88fd449c5ac6c46da0056482f29538
adityadev1 <adityadev097@gmail.com>
2016-09-19 18:56:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/page-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
