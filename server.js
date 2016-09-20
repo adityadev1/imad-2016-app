0dcc90758cb250db17883f1f25525a1e4d2a8e80
adityadev1 <adityadev097@gmail.com>
2016-09-20 09:17:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleone));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send(createTemplate(articletwo));
 });
 
